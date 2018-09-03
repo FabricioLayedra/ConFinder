5c030d370d2736f08b23b330765b10162b51a151
AnibalG <aagamboa@espol.edu.ec>
2018-09-02 21:38:29 -0500

    restringiendo campos

--- Conferencias/API/serializer.py
+++ Conferencias/API/serializer.py
@@ -6,3 +6,11 @@ class EventosSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Eventos
         fields = '__all__'
+
+        extra_kwargs = {'Name': {'required': False, "read_only": True},
+                        'acronym': {'required': False, "read_only": True},
+                        'topics': {'required': False, "read_only": True},
+                        'start_date': {'required': False, "read_only": True},
+                        'deadline_date': {'required': False, "read_only": True},
+                        'location': {'required': False, "read_only": True}
+                        }
